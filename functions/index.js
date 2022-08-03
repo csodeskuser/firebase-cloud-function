21bab651375e069de40c0b16fd66785228d3656f
csodeskuser <csodeskuser@gmail.com>
2022-08-03 11:13:02 +0530

    cord issue fixed

--- functions/index.js
+++ functions/index.js
@@ -7,93 +7,104 @@ const db = admin.firestore();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+const cors = require('cors')({origin: true});
 
 /* Create New Contact */
 exports.createContacts = functions.https.onRequest( async (req, res) => {
-  db.collection("contacts").add(req.body).then((output)=>{
-    res.json({
-      status:true,
-      result:output.id
+  cors(req, res, () => {
+    db.collection("contacts").add(req.body).then((output)=>{
+      res.json({
+        status:true,
+        result:output.id
+      });
+    }).catch((err)=>{
+      res.json({
+        status:false,
+        result:err
+      });
     });
-   }).catch((err)=>{
-    res.json({
-      status:false,
-      result:err
-    });
-   });
+  });
 });
 
 /* Get All Contact */
 exports.getContacts = functions.https.onRequest( async (req, res) => {
-  db.collection('contacts').get().then((output)=>{
-    let contactData = [];
-    output.forEach(doc => {
-      let contactObj = doc.data();
-      contactObj.id = doc.id;
-      contactData.push(contactObj);
-    });
-    if(contactData.length > 0) {
-      res.json({
-        status:true,
-        result:contactData,
-        data:"1"
+  cors(req, res, () => {
+    db.collection('contacts').get().then((output)=>{
+      let contactData = [];
+      output.forEach(doc => {
+        let contactObj = doc.data();
+        contactObj.id = doc.id;
+        contactData.push(contactObj);
       });
-    } else {
+      if(contactData.length > 0) {
+        res.json({
+          status:true,
+          result:contactData,
+          data:"1"
+        });
+      } else {
+        res.json({
+          status:false,
+          result:"Contact Not Found",
+        });
+      }
+      
+    }).catch((err)=>{
       res.json({
         status:false,
-        result:"Contact Not Found",
+        result:err
       });
-    }
-    
-  }).catch((err)=>{
-    res.json({
-      status:false,
-      result:err
     });
   });
 });
 
 /* Get Single Contact */
 exports.getContact = functions.https.onRequest( async (req, res) => {
-  db.collection('contacts').doc(req.params[0]).get().then((output)=>{
-    res.json({
-      status:true,
-      result:output.data()
-    });
-  }).catch((err)=>{
-    res.json({
-      status:false,
-      result:err
+  cors(req, res, () => {
+    db.collection('contacts').doc(req.params[0]).get().then((output)=>{
+      res.json({
+        status:true,
+        result:output.data()
+      });
+    }).catch((err)=>{
+      res.json({
+        status:false,
+        result:err
+      });
     });
   });
 });
 
 /* Update Contact */
 exports.updateContact = functions.https.onRequest( async (req, res) => {
-  db.collection('contacts').doc(req.params[0]).set(req.body).then(()=>{
-    res.json({
-      status:true,
-      result:"Success"
-    });
-  }).catch((err)=>{
-    res.json({
-      status:false,
-      result:err
+  cors(req, res, () => {
+    db.collection('contacts').doc(req.params[0]).set(req.body).then(()=>{
+      res.json({
+        status:true,
+        result:"Success"
+      });
+    }).catch((err)=>{
+      res.json({
+        status:false,
+        result:err
+      });
     });
   });
 });
 
 /* Delete Contact */
 exports.deleteContact = functions.https.onRequest( async (req, res) => {
-  db.collection('contacts').doc(req.params[0]).delete().then(()=>{
-    res.json({
-      status:true,
-      result:"Success"
-    });
-  }).catch((err)=>{
-    res.json({
-      status:false,
-      result:err
+  cors(req, res, () => {
+    db.collection('contacts').doc(req.params[0]).delete().then(()=>{
+      res.json({
+        status:true,
+        result:"Success"
+      });
+    }).catch((err)=>{
+      res.json({
+        status:false,
+        result:err
+      });
     });
   });
 });
